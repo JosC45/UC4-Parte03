333138f06ea049926ce819aed883fbbe1061bffc
Axel <20b3010455@untels.edu.pe>
2023-12-13 10:06:29 -0500

    CRUD CURSOS - Listo

--- proyecto/views.py
+++ proyecto/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Course
 
 def cursos(request):
-    return render(request, 'cursos.html', {})
+    cursosListado = Course.objects.all()
+    return render(request, 'cursos.html', {"curso": cursosListado})
 
 def crear_curso(request):
     return render(request, 'crear_curso.html', {})
@@ -12,9 +14,20 @@ def carreras(request):
 
 def crear_carrera(request):
     return render(request, 'crear_carrera.html', {})
-from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
 
-from django.shortcuts import render
+def eliminar_curso(request,idcourse):
+    curso=Course.objects.get(idcourse=idcourse)
+    curso.delete()
+    return redirect('/cursos/')
+
+def registrar_curso(request):
+    code=request.POST['code']
+    name=request.POST['name']
+    hour=request.POST['hour']
+    credits=request.POST['credits']
+    state=request.POST['state']
+    curso=Course.objects.create(code=code,name=name,hour=hour,credits=credits,state=state)
+    return redirect('/crear_curso/')
